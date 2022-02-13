3e258af90189403d45fac1d80f58bea7045cb6ad
hoangqwe159 <41616812+hoangqwe159@users.noreply.github.com>
2022-02-13 19:25:07 +1000

    calculate interest

--- leetcode/main.js
+++ leetcode/main.js
@@ -17,14 +17,63 @@ const twoSum = function (numbers, target) {
 // Description: Best Time to Buy and Sell Stock
 // Difficulty: Easy
 // Solution:
-const maxProfit = function(prices) {
-    let maxPrice = - Infinity;
-		let minPrice = Infinity;
-		for (let i = 0; i < prices; i++) {
-			if (prices[i] > maxPrice) {
-				maxPrice = prices[i];
-			}
-			if (prices[i] < minPrice)
-		}
+// const maxProfit = function(prices) {
+//     let maxPrice = - Infinity;
+// 		let minPrice = Infinity;
+// 		for (let i = 0; i < prices; i++) {
+// 			if (prices[i] > maxPrice) {
+// 				maxPrice = prices[i];
+// 			}
+// 			if (prices[i] < minPrice)
+// 		}
 
-};
+// };
+
+const continuousCompoundInterest = (fv, percent, duration) => {
+	let total = 0;
+	for (let i = 0; i < duration; i++) {
+		total = (total + fv) * (1 + percent);
+	}
+	return total;
+}
+
+const compoundInterest = (fv, percent, duration) => {
+	return fv * Math.pow((1 + percent), duration);
+}
+console.log(compoundInterest(10, 0.15, 20));
+
+let investPerYear = 360; // million VND
+let profitPerYear = 0.1; // 10%
+let durationYear = 8; // 8 years
+
+let investPerMonth = investPerYear / 12;
+let profitPerMonth = profitPerYear /12;
+let durationMonth =durationYear * 12;
+
+// invest monthly
+console.log(continuousCompoundInterest(investPerMonth, profitPerMonth,durationMonth));
+
+// invest annually
+console.log(continuousCompoundInterest(investPerYear, profitPerYear, durationYear));
+
+// saving
+console.log(investPerYear * durationYear);
+
+
+// Invest 20 years, 6% per year
+investPerYear = 360;
+profitPerYear = 0.06;
+durationYear = 20;
+
+investPerMonth = investPerYear / 12;
+profitPerMonth = profitPerYear /12;
+durationMonth =durationYear * 12;
+
+// invest monthly
+console.log(continuousCompoundInterest(investPerMonth, profitPerMonth,durationMonth));
+
+// invest annually
+console.log(continuousCompoundInterest(investPerYear, profitPerYear, durationYear));
+
+// saving
+console.log(investPerYear * durationYear);
